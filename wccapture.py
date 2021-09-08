6cb71a3dfd689e3b2177d087bbc28a3f98106773
anhong <anhonghe@gmail.com>
2021-09-08 12:44:20 -0700

    working

--- wccapture.py
+++ wccapture.py
@@ -1,69 +1,18 @@
 import time
-import smtplib
-import ssl
-import urllib.request
+import _thread
 
-from io import BytesIO
+from tkinter import Tk
+from tkinter import Button
+from tkinter import Label
+from tkinter import StringVar
+
 from PIL import ImageGrab
 from PIL import ImageChops
 from datetime import datetime
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
-
-# send the wechat window capture and send to email
-# copied from: https://realpython.com/python-send-email/
-def sendImage(img):
-    sendSelfEmail = 'xxx@gmail.com'
-    sendSelfPassword = 'xxx'
-
-    sendEmail = sendSelfEmail
-    sendPassword = sendSelfPassword
-
-    recvEmail = sendSelfEmail
-    sendSubject = datetime.now().strftime("wcspy.%d.%m.%Y.%H.%M.%S.jpg")
-
-    # Create a multipart message and set headers
-    message = MIMEMultipart()
-    message["From"] = sendEmail
-    message["To"] = recvEmail
-    message["Subject"] = sendSubject
-    message["Bcc"] = recvEmail  # Recommended for mass emails
-
-    # Add body to email
-    message.attach(MIMEText("auto send by wcspy", "plain"))
-
-    # Convert the img to image file in memory
-    imgf = BytesIO()
-    img.save(imgf, "JPEG", quality=50)
-    imgf.seek(0)
-
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(imgf.read())
-
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
-
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {sendSubject}",
-    )
-
-    # Add attachment to message and convert message to string
-    message.attach(part)
-    text = message.as_string()
-
-    # Log in to server using secure context and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sendEmail, sendPassword)
-        server.sendmail(sendEmail, recvEmail, text)
-
+g_text = None
+g_exit = None
+g_button = None
 
 # detect the wechat window
 # copied from https://blog.csdn.net/fujliny/article/details/109411343
@@ -124,27 +73,75 @@ def captureWCWindow():
     return img.crop((upPixStartIndex % width, upPixStartIndex // width, downPixEndIndex % width, downPixEndIndex // width))
 
 
-def main():
+def captureLoop():
+    global g_text
+    global g_exit
+    global g_button
+
     lastImg = None
     currImg = None
 
-    while True:
+    g_exit = False
+    g_button.config(text='stop')
+
+    while not g_exit:
         try:
+            currImg = captureWCWindow()
             if lastImg is None or currImg is None:
                 # didn't get anything
-                sleepSec = min(sleepSec + 1, 5)
+                g_text.set("No wc window detected")
             else:
                 diff = ImageChops.difference(lastImg, currImg)
                 if diff.getbbox():
-                    sendImage(currImg)
-                    sleepSec = max(sleepSec - 1, 1)
+                    labelValue = datetime.now().strftime("wcspy.%d.%m.%Y.%H.%M.%S.%f.jpg")
+                    g_text.set('Saved: ' + labelValue)
+                    currImg.save(labelValue)
+                else:
+                    g_text.set("Window content not changed")
+
         except:
             # mute any error
             # don't stop the program anyway
             pass
 
         lastImg = currImg
-        time.sleep(sleepSec)
+        time.sleep(0.1)
+
+    g_exit = None
+    g_text.set("inactive")
+    g_button.config(text='start')
+
+
+def buttonCallback():
+    global g_text
+    global g_exit
+    global g_button
+
+    if g_exit is None or g_exit is True:
+        g_exit = False
+        _thread.start_new_thread(captureLoop, ())
+    else:
+        g_exit = True
+
+
+def main():
+    global g_text
+    global g_exit
+    global g_button
+
+    top = Tk()
+    top.geometry("400x100")
+
+    g_text = StringVar()
+    g_text.set("inactive")
+
+    g_button = Button(top, text="start", command=buttonCallback)
+    g_button.place(x=0, y=0)
+
+    label = Label(top, textvariable=g_text)
+    label.pack()
+
+    top.mainloop()
 
 
 if __name__ == '__main__':
